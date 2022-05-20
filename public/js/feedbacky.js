35d7bd03b17d34dd120dd6a30d2a0413df139c2a
berkekurnaz <bkoyunberkekurnaz@hotmail.com>
2022-05-20 13:03:19 +0300

    some changes

--- public/js/feedbacky.js
+++ public/js/feedbacky.js
@@ -1,49 +1,57 @@
-if(feedbackyData){
+if (feedbackyData) {
     document.body.innerHTML += feedbackyData.feedbackyContent;
     var modal = document.getElementById("feedbackyModal");
     var modalInsideContent = document.getElementsByClassName("feedbacky-modal-inside-content")[0];
     var defaultModalInsideContent = modalInsideContent.innerHTML;
     var span = document.getElementsByClassName("feedbacky-close")[0];
-}else{
+} else {
     console.log("Please, add your feedbacky datas.");
 }
 
 var API_URL = "https://feedbacky-backend-2022.herokuapp.com/form/createAnswer";
 
-function feedbackyModalOpen(){
+function feedbackyModalOpen() {
     modalInsideContent.innerHTML = defaultModalInsideContent;
     modal.style.display = "block";
 };
 
-function feedbackyModalClose(){
+function feedbackyModalClose() {
     modal.style.display = "none";
 };
 
-function feedbackySendForm(){
+function feedbackySendForm() {
     var formBody = [];
     var formBodyFields = [];
 
     feedbackyData.formAreas.forEach(area => {
-          formBodyFields.push({
+        var areaName = document.getElementsByClassName(area.name)[0].classList.value;
+        var areaAnswer = document.getElementsByClassName(area.name)[0].value;
+        if (area.minLength < areaAnswer.length && area.maxLength > areaAnswer.length) {
+            formBodyFields.push({
                 fieldName: document.getElementsByClassName(area.name)[0].classList.value,
                 fieldAnswer: document.getElementsByClassName(area.name)[0].value
-          });
+            });
+        } else {
+            document.getElementsByClassName(area.name)[0].value = "Please, write something here..."
+        }
     });
-    const Http = new XMLHttpRequest();
-    Http.open("POST", API_URL);
-    Http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    formBody = {
-          response: JSON.stringify(formBodyFields),
-          userAgent: navigator.userAgent,
-          formId: feedbackyData.feedbackyID
-    };
-    Http.send(JSON.stringify(formBody));
+    if (formBodyFields.length > 0) {
+        const Http = new XMLHttpRequest();
+        Http.open("POST", API_URL);
+        Http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        formBody = {
+            response: JSON.stringify(formBodyFields),
+            userAgent: navigator.userAgent,
+            formId: feedbackyData.feedbackyID
+        };
+        Http.send(JSON.stringify(formBody));
 
-    Http.onreadystatechange = (e) => {
-          if(Http.status === 201){
+        Http.onreadystatechange = (e) => {
+            if (Http.status === 201) {
                 modalInsideContent.innerHTML = feedbackyData.feedbackyResultContent;
-          }
-    };
+            }
+        };
+    }
 };
 
 span.onclick = function () {
@@ -52,7 +60,7 @@ span.onclick = function () {
 
 window.onclick = function (event) {
     if (event.target == modal) {
-          modal.style.display = "none";
+        modal.style.display = "none";
     }
 };
 
